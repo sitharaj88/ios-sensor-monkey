96e361071f6bf5b5aa2cd3e7d666c8684bc4f0cb
Sitharaj <sitharaj.info@gmail.com>
2025-07-16 06:53:04 +0530

    Initial Commit

---
+++ sensor monkey/sensor_monkeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  sensor_monkeyApp.swift
+//  sensor monkey
+//
+//  Created by Sitharaj Seenivasan on 16/07/25.
+//
+
+import SwiftUI
+
+@main
+struct sensor_monkeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sensor monkeyTests/sensor_monkeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  sensor_monkeyTests.swift
+//  sensor monkeyTests
+//
+//  Created by Sitharaj Seenivasan on 16/07/25.
+//
+
+import Testing
+@testable import sensor_monkey
+
+struct sensor_monkeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
